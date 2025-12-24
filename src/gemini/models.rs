c92adeab200ad56f091f6132162b6781f89d13d8
sosin <svstar94@naver.com>
2025-12-24 14:06:49 +0900

    feat: update models

--- src/gemini/models.rs
+++ src/gemini/models.rs
@@ -1,17 +1,20 @@
 use serde::Deserialize;
 #[derive(Deserialize, Debug)]
 pub enum GeminiModel {
-    #[serde(rename(deserialize = "gemini-2.5-flash"))]
-    Gemini25Flash,
     #[serde(rename(deserialize = "gemini-3-pro-preview"))]
     Gemini3ProPreview,
+    // $2 / $12 (<200,000 tokens)
+    // $4 / $18 (>200,000 tokens)
+    #[serde(rename(deserialize = "gemini-3-flash-preview"))]
+    Gemini3FlashPreview,
+    // $0.50 / $3
 }
 
 impl ToString for GeminiModel {
     fn to_string(&self) -> String {
         match self {
-            GeminiModel::Gemini25Flash => "gemini-2.5-flash",
             GeminiModel::Gemini3ProPreview => "gemini-3-pro-preview",
+            GeminiModel::Gemini3FlashPreview => "gemini-3-flash-preview",
         }
         .into()
     }
